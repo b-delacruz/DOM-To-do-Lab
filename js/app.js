a238bb8400e98740ed3ac45f4bc9fa806e0662aa
b-delacruz <brianmdelacruz@gmail.com>
2022-05-31 17:05:36 -0400

    add ID to elements

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// cached elements
+
+const submitButton = document.querySelector("#submit-button")
+const firstId = document.querySelector("#first-id")
+const doList = document. querySelector("#todo-list")
+// 
+
+
