c013a65d2713ed790041a77a7cf22d0b380a46e8
b-delacruz <brianmdelacruz@gmail.com>
2022-05-31 17:07:11 -0400

    finish dom lab pt 1

--- js/app.js
+++ js/app.js
@@ -7,6 +7,10 @@ const doList = document. querySelector("#todo-list")
 
 
 submitButton.addEventListener('click',function(evt){
-  
+  const li = document.createElement('li')
+  const inp = document.querySelector('input')
+  li.textContent = inp.value
+  document.querySelector('ul').appendChild(li)
+  inp.value = ''
 })
 
